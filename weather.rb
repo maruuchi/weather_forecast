5610ca342d2d0079de781cbdacc4830eca1e1a38
soma-maruuchi <mwa60346@gmail.com>
2020-08-28 08:05:35 +0900

    dateを追加

--- weather.rb
+++ weather.rb
@@ -1,7 +1,13 @@
 require "csv"
 require "json"
+require "date"
+require "pry"
 
 
 # 実行は ruby weather.rb
+
+print "住んでいる地域を以下から選択してください"
 # 佐賀県の地域を表示
 CSV.read("sagapre.csv")
+
+puts "No.#{i}, #{local[1]}"
